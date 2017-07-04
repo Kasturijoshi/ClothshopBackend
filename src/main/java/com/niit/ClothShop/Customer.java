e8cae66c9844c17bdc3363264c148a76b3471044
Kasturijoshi <kasturi21994@gmail.com>
2017-07-04 17:07:11 +0530

    first commit

---
+++ src/main/java/com/niit/ClothShop/Customer.java
@@ -0,0 +1,53 @@
+package com.niit.ClothShop;
+
+public class Customer {
+String cusid,cname,cuspswrd,cusemail,cusadd,cusphn;
+
+public String getCusid() {
+	return cusid;
+}
+
+public void setCusid(String cusid) {
+	this.cusid = cusid;
+}
+
+public String getCname() {
+	return cname;
+}
+
+public void setCname(String cname) {
+	this.cname = cname;
+}
+
+public String getCuspswrd() {
+	return cuspswrd;
+}
+
+public void setCuspswrd(String cuspswrd) {
+	this.cuspswrd = cuspswrd;
+}
+
+public String getCusemail() {
+	return cusemail;
+}
+
+public void setCusemail(String cusemail) {
+	this.cusemail = cusemail;
+}
+
+public String getCusadd() {
+	return cusadd;
+}
+
+public void setCusadd(String cusadd) {
+	this.cusadd = cusadd;
+}
+
+public String getCusphn() {
+	return cusphn;
+}
+
+public void setCusphn(String cusphn) {
+	this.cusphn = cusphn;
+}
+}
---
+++ src/main/java/com/niit/ClothShop/Supplier.java
@@ -0,0 +1,29 @@
+package com.niit.ClothShop;
+
+public class Supplier {
+String supid,supname,supadd;
+
+public String getSupid() {
+	return supid;
+}
+
+public void setSupid(String supid) {
+	this.supid = supid;
+}
+
+public String getSupname() {
+	return supname;
+}
+
+public void setSupname(String supname) {
+	this.supname = supname;
+}
+
+public String getSupadd() {
+	return supadd;
+}
+
+public void setSupadd(String supadd) {
+	this.supadd = supadd;
+}
+}
